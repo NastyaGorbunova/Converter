ce2ba97921f17bcd5c1366af0d4471ae7bab4d84
Nastya Gorbunova <nastassia.gorbunova@gmail.com>
2018-12-14 00:34:48 +0300

    HomeWork

--- Converter/Program.cs
+++ Converter/Program.cs
@@ -4,113 +4,127 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Converter
+namespace CalculateWay
 {
-    class Program
+    class CalculateWay
     {
 
         static void Main(string[] args)
         {
-            const byte ZeroAnscii = 48;
-            const byte NineAnscii = 57;
-            const byte AAnscii = 65;
-            const byte FAnscii = 70;
-            const byte aAnscii = 97;
-            const byte fAnscii = 102;
+            Point3D startPoint  = new Point3D ( 0, 0, 0 );
+            Point3D endPoint = new Point3D(100, 200, 800);
 
-            Console.Write("Enter the number in hexadecimal system:");
-            string input = Console.ReadLine();
+            Bird newBird = new Bird();
+            Plane newPlane = new Plane();
+            SpaceShip newSpaceShip = new SpaceShip();
 
-            //Валидация:
-            byte[] AnsciiCode = Encoding.ASCII.GetBytes(input);
-            foreach (byte symbol in AnsciiCode)
-            {
-                if ((symbol >= ZeroAnscii && symbol <= NineAnscii)
-                    || (symbol >= AAnscii && symbol <= FAnscii)
-                    || (symbol >= aAnscii && symbol <= fAnscii))
-                    Console.ReadLine();
-                else
-                {
-                    break;
-                }
-            }
-
-            Methods Conversion = new Methods();
-            int input_dec = Conversion.ConvertHexToDec(input);
-            Console.WriteLine("Your number in decimal: " + input);
+            Console.WriteLine(newBird.WhoAmI() + ": " + newBird.FlyTo(startPoint.Coordinates, endPoint.Coordinates) + ", " + newBird.GetFlyTime(startPoint.Coordinates, endPoint.Coordinates) + "km/hour");
+            Console.WriteLine(newPlane.WhoAmI() + ": " + newPlane.FlyTo(startPoint.Coordinates, endPoint.Coordinates) + ", " + newPlane.GetFlyTime(startPoint.Coordinates, endPoint.Coordinates) + "km/hour");
+            Console.WriteLine(newSpaceShip.WhoAmI() + ": " + newSpaceShip.FlyTo(startPoint.Coordinates, endPoint.Coordinates) + ", " + newSpaceShip.GetFlyTime(startPoint.Coordinates, endPoint.Coordinates) + "km/hour");
             Console.ReadLine();
-
-            string input_bin = Conversion.ConvertDecToBinary(input);
-            Console.WriteLine("Your number in binary: " + input);
-            Console.ReadLine();
-
         }
     }
 
-    class Methods
+    public struct Point3D
     {
-        const byte AAnscii = 65; const byte aAnscii = 97;
-        const byte BAnscii = 66; const byte bAnscii = 98;
-        const byte CAnscii = 67; const byte cAnscii = 99;
-        const byte DAnscii = 68; const byte dAnscii = 100;
-        const byte EAnscii = 69; const byte eAnscii = 101;
-        const byte FAnscii = 70; const byte fAnscii = 102;
+        public int[] Coordinates { get; }
 
-        public int ConvertHexToDec (string input_hex)
+        public Point3D (int x, int y, int z)
         {
-            char[] char_input = input_hex.ToCharArray();
-            byte[] AnsciiInput = Encoding.ASCII.GetBytes(input_hex);
+            int[] Coordinates = new int[] { x, y, z };
+        }
+    }
 
-            for (int i = 0; i < input_hex.Length; i++)
-            {
-                if (AnsciiInput[i] == AAnscii)
-                    char_input[i].Equals(10);
-                if (AnsciiInput[i] == AAnscii)
-                    char_input[i].Equals(11);
-                if (AnsciiInput[i] == AAnscii)
-                    char_input[i].Equals(12);
-                if (AnsciiInput[i] == AAnscii)
-                    char_input[i].Equals(13);
-                if (AnsciiInput[i] == AAnscii)
-                    char_input[i].Equals(14);
-                if (AnsciiInput[i] == AAnscii)
-                    char_input[i].Equals(15);
-            }
+    class CalcDistance
+    {
+        public double Distance (int[] startPoint, int[] endPoint)
+        {
+            double distance = 0;
+            for (int i = 0; i < startPoint.Length; i++)
+            { distance = distance + (Math.Pow((endPoint[i] - startPoint[i]), 2)); }
+            distance = Math.Sqrt(distance);
 
-            int output = input_hex[0];
+            return distance;
+        }
+    }
 
-            for (int i = 1; i < input_hex.Length; i++)
-            {
-                output = output + output * 16 + input_hex[i];
-            }
+    interface IFlyable
+    {
+        double FlyTo(int[] Point, int[] newPoint);
 
-            return output;
+        string WhoAmI();
+
+        double GetFlyTime(int[] Point, int[] newPoint);
+
+    }
+
+
+    class Bird : IFlyable
+    {
+        public double FlyTo(int[] startPoint, int[] endPoint)
+        {
+            CalcDistance calcDist = new CalcDistance();
+            return calcDist.Distance(startPoint, endPoint);
         }
 
-        public string ConvertDecToBinary(string input_hex)
+        public string WhoAmI()
         {
-            Methods ToBinary = new Methods();
-            int input_dec = ToBinary.ConvertHexToDec(input_hex);
-            int leftover = input_dec;
-            int temp = 0;
-            string Output = "";
+            return "bird";
+        }
 
-            while (leftover > 0)
+        public double GetFlyTime(int[] Point, int[] newPoint)
+        {
+            Random birdSpeed = new Random();
+            return birdSpeed.Next(0, 20);
+        }
+    }
+
+    class Plane : IFlyable
+    {
+        public double FlyTo(int[] startPoint, int[] endPoint)
+        {
+            CalcDistance calcDist = new CalcDistance();
+            return calcDist.Distance(startPoint, endPoint);
+        }
+
+        public string WhoAmI()
+        {
+            return "plane";
+        }
+
+        public double GetFlyTime(int[] startPoint, int[] endPoint)
+        {
+            double distance = FlyTo(startPoint, endPoint);
+            double speedChange = 10;
+            double planeSpeed = 200;
+
+            double distCoef = distance/10;
+            for (int i = 0; i < distCoef; i++)
             {
-                temp = leftover % 2;
-                leftover = leftover / 2;
-                Output = Output + temp;
-            }
-            /*
-            char[] OutputChar = Output.ToArray();
-            string OutputBinary = "";
-            for (int i = Output.Length; i >= 0; i--)
-            {
-                OutputBinary = (OutputChar[i]);
+                planeSpeed = planeSpeed + speedChange;
             }
 
-            return OutputBinary;
-        }*/
-            return Output;
+            return planeSpeed;
+        }
+    }
+
+    class SpaceShip : IFlyable
+    {
+        public double FlyTo(int[] startPoint, int[] endPoint)
+        {
+            CalcDistance calcDist = new CalcDistance();
+            return calcDist.Distance(startPoint, endPoint);
+        }
+
+        public string WhoAmI()
+        {
+            return "spaceship";
+        }
+
+        public double GetFlyTime(int[] Point, int[] newPoint)
+        {
+            double spaceShipSpeed = 28800000;
+            return spaceShipSpeed;
+        }
     }
 }
